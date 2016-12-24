1e7f807faccd9a718f00526676c3a489a68354b5
Tang Rufus <tangrufus@gmail.com>
2016-12-24 09:39:56 +0800

    wp scaffold plugin wp-rocket-server-push --plugin_name='WP Rocket Server Push' --plugin_author='Tang Rufus'

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wp_Rocket_Server_Push
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ wp-rocket-server-push.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WP Rocket Server Push
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Tang Rufus
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-rocket-server-push
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Rocket_Server_Push
+ */
